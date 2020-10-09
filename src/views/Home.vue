65703ee50043945b56555ed415def0f584e4945b
unknown <faker.top@qq.com>
2020-10-09 17:33:40 +0800

    修改HOME.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <h1>RNG电子竞技俱乐部 英雄联盟分部 RNG-letme 上单职业选手</h1>
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
